781da47cec50d8c9839562490df963db166e933a
Mark Hunter <markhunter2005@gmail.com>
2018-05-01 21:05:30 -0400

    new

---
+++ Java Code/src/MyStrings.java
@@ -0,0 +1,32 @@
+public class MyStrings {
+
+  private void insert(char[] str, int index, String toAdd) {
+    for (char c : toAdd.toCharArray()) {
+      str[index] = c;
+      index++;
+    }
+  }
+
+  //1.3 cracking the code
+  public void urlify(char[] str, int len){
+    int spaces = 0; // number of spaces in str
+    for (int i = len - 1; i >= 0; i--) {
+      if (str[i] == ' ') {
+        spaces ++;
+      }
+    }
+    int shift = spaces * 2;
+    for (int i = len - 1; i >= 0 && shift >= 0; i--) {
+      char curChar = str[i];
+      //System.out.println(curChar);
+      if (curChar == ' '){
+        shift -= 2;
+        insert(str, i + shift, "%20");
+
+      } else {
+        //System.out.printf("i = %d, shift_amt = %d currChar = %c \n",i, shift, curChar  );
+        str[i+shift] = curChar;
+      }
+    }
+  }
+}
---
+++ Java Code/test/Tests.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Tests {
+  MyStrings obj = new MyStrings();
+
+  @Test
+  public void testCat(){
+    char[] test = {'c', ' ', 'a', ' ', 't', ' ', ' ', ' ', ' '};
+    obj.urlify(test, 5);
+    String rv = new String(test);
+    assertEquals("c%20a%20t", rv);
+  }
+
+  @Test
+  public void testSimple() {
+    char[] empty = {};
+    obj.urlify(empty, 0);
+    String rv = new String(empty);
+    assertEquals("", rv);
+  }
+
+  @Test
+  public void testSimple2() {
+    char[] mark = {'m', 'a', 'r', 'k'};
+    obj.urlify(mark, 4);
+    String rv = new String(mark);
+    assertEquals("mark", rv);
+  }
+
+  @Test
+  public void testBookExample() {
+    char[] ex = {'M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', ' ', ' ', ' ',
+            ' '};
+    obj.urlify(ex, 13);
+    String rv = new String(ex);
+    assertEquals("Mr%20John%20Smith", rv);
+  }
+
+}
