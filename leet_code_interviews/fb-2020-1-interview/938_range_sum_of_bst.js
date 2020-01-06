0ea88ff5b9637ddc22d28f1d8ff7c22216f68bae
Mark Hunter <hunter.mark@husky.neu.edu>
2020-01-06 11:51:26 -0500

    more effiencet solition

--- leet_code_interviews/fb-2020-1-interview/938_range_sum_of_bst.js
+++ leet_code_interviews/fb-2020-1-interview/938_range_sum_of_bst.js
@@ -13,15 +13,16 @@
  * @param {number} R
  * @return {number}
  */
-var rangeSumBST = function(root, L, R) {
-    if (root === null) {
-        return 0;
-    } else if (root.val >= L && root.val <= R ) {
-        return root.val
-            + rangeSumBST(root.left, L, R)
-            + rangeSumBST(root.right, L, R);
-    } else {
-        return rangeSumBST(root.left, L, R)
-            + rangeSumBST(root.right, L, R)
-    }
-};
+ var rangeSumBST = function(root, L, R) {
+     if (root === null) {
+         return 0;
+     } else if (root.val >= L && root.val <= R ) {
+         return root.val
+             + rangeSumBST(root.left, L, R)
+             + rangeSumBST(root.right, L, R);
+     } else if (root.val < L) {
+         return rangeSumBST(root.right, L, R);
+     } else if (root.val > R) {
+         return rangeSumBST(root.left, L, R);
+     }
+ };
